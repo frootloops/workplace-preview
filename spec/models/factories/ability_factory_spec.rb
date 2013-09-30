433f5ad529d725f3249773168471fdb8a4d97c05
Arsen Gasparyan <becode21@gmail.com>
2013-09-30 19:24:48 +0400

    Added admin_menu

--- spec/models/factories/ability_factory_spec.rb
+++ spec/models/factories/ability_factory_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe AbilityFactory do
   let(:guest) { AbilityFactory.build build(:guest) }
+  let(:empty) { AbilityFactory.build nil }
   let(:client) { AbilityFactory.build create(:client) }
   let(:admin) { AbilityFactory.build create(:admin) }
 
   it { expect(guest).to be_a_kind_of(GuestAbility) }
+  it { expect(empty).to be_a_kind_of(GuestAbility) }
   it { expect(client).to be_a_kind_of(ClientAbility) }
   it { expect(admin).to be_a_kind_of(AdminAbility) }
 end
